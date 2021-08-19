04dcdde8cabc355349e2983683b3a9ef987c5e2d
Hugo Pérard <hugo.perard@bearstudio.fr>
2021-08-19 21:41:20 +0200

    fix not logged screen appear on refresh

--- src/app/auth/AuthProvider.tsx
+++ src/app/auth/AuthProvider.tsx
@@ -3,12 +3,9 @@ import { useState } from 'react';
 import firebase from '@/firebase';
 
 import { PageLogin } from './PageLogin';
-import { useAuth } from './useAuth';
 
 export const AuthProvider = ({ children }) => {
-  const { isLogged: isLoggedAuth } = useAuth();
-
-  const [isLogged, setIsLogged] = useState(isLoggedAuth);
+  const [isLogged, setIsLogged] = useState(true);
 
   firebase.auth().onIdTokenChanged((user) => {
     if (!user || !user.email.endsWith('@bearstudio.fr')) {
