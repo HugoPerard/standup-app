a057a53e5adc19ab65c0dcf3dee1be7093391334
Hugo Pérard <hugo.perard@bearstudio.fr>
2021-08-19 11:48:41 +0200

    PageOffices day text center and uppercase + add spacing

--- src/app/offices/PageOffices.tsx
+++ src/app/offices/PageOffices.tsx
@@ -12,7 +12,7 @@ import {
 import { Office, OfficeWorker } from './offices.types';
 
 export const PageOffices = () => {
-  const weekdays = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi'];
+  const weekdays = ['LUNDI', 'MARDI', 'MERCREDI', 'JEUDI', 'VENDREDI'];
 
   const { data: offices } = useOffices();
 
@@ -52,10 +52,15 @@ export const PageOffices = () => {
   return (
     <Page containerSize="full">
       <PageContent>
-        <Stack spacing={4}>
+        <Stack spacing={6}>
           {weekdays?.map((weekday) => (
             <Stack key={weekday}>
-              <Text fontSize="lg" fontWeight="medium" color="yellow.500">
+              <Text
+                fontSize="lg"
+                fontWeight="bold"
+                color="yellow.500"
+                alignSelf="center"
+              >
                 {weekday}
               </Text>
               <Stack direction={{ base: 'column', md: 'row' }}>
@@ -72,56 +77,46 @@ export const PageOffices = () => {
                         {office?.name}
                       </Text>
                       <Badge variant="solid" px={2}>
-                        {office?.presence[weekday.toUpperCase()]?.length}
+                        {office?.presence[weekday]?.length}
                       </Badge>
                     </HStack>
                     <Stack direction="row" flex={1}>
                       <OfficeSection
                         title="Journée"
-                        presence={office?.presence[
-                          weekday.toUpperCase()
-                        ]?.filter(
+                        presence={office?.presence[weekday]?.filter(
                           (person) => person?.onMorning && person?.onAfternoon
                         )}
-                        onClick={() =>
-                          handleChangePresence(office, weekday.toUpperCase())
-                        }
+                        onClick={() => handleChangePresence(office, weekday)}
                         flex={1}
                       />
                       <Divider orientation="vertical" />
                       <Stack flex={1}>
                         <OfficeSection
                           title="Matin"
-                          presence={office?.presence[
-                            weekday.toUpperCase()
-                          ]?.filter(
+                          presence={office?.presence[weekday]?.filter(
                             (person) =>
                               person?.onMorning && !person?.onAfternoon
                           )}
                           onClick={() =>
-                            handleChangePresence(
-                              office,
-                              weekday.toUpperCase(),
-                              { onMorning: true, onAfternoon: false }
-                            )
+                            handleChangePresence(office, weekday, {
+                              onMorning: true,
+                              onAfternoon: false,
+                            })
                           }
                           flex={1}
                         />
                         <Divider />
                         <OfficeSection
                           title="Après-midi"
-                          presence={office?.presence[
-                            weekday.toUpperCase()
-                          ]?.filter(
+                          presence={office?.presence[weekday]?.filter(
                             (person) =>
                               !person?.onMorning && person?.onAfternoon
                           )}
                           onClick={() =>
-                            handleChangePresence(
-                              office,
-                              weekday.toUpperCase(),
-                              { onMorning: false, onAfternoon: true }
-                            )
+                            handleChangePresence(office, weekday, {
+                              onMorning: false,
+                              onAfternoon: true,
+                            })
                           }
                           flex={1}
                         />
