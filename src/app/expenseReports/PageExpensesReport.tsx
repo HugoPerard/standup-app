1afcb9ab575022abe8480a29dd03ab7f0b163333
Philippe Chevreul <philippe@bearstuido.fr>
2021-09-01 14:01:58 +0200

    feat: field day picker component

--- src/app/expenseReports/PageExpensesReport.tsx
+++ src/app/expenseReports/PageExpensesReport.tsx
@@ -1,9 +1,51 @@
-import { Page, PageContent } from '@/app/layout';
+import { forwardRef } from 'react';
 
-export const PageExpensesReport = () => {
+import { Stack, StackProps } from '@chakra-ui/react';
+import { Formiz } from '@formiz/core';
+
+import { useCurrentUser } from '@/app/auth/useAuth';
+import { Page, PageContent } from '@/app/layout';
+import { FieldInput } from '@/components/FieldInput';
+import { FieldTextarea } from '@/components/FieldTextarea';
+
+interface GoalFormProps extends StackProps {}
+
+export const PageExpensesReport = forwardRef<HTMLElement, GoalFormProps>(() => {
+  const currentUser = useCurrentUser();
+
   return (
     <Page containerSize="full">
-      <PageContent>Notes de frais | WIP</PageContent>
+      <PageContent>
+        <Formiz
+          autoForm
+          onValidSubmit={() => {}}
+          initialValues={{
+            name: currentUser.username,
+          }}
+        >
+          <Stack spacing={3}>
+            <FieldInput
+              name="name"
+              label="Nom"
+              placeholder="Votre nom et prénom"
+              required="Votre nom est requis"
+              isDisabled
+            />
+            <FieldInput
+              name="month"
+              label="Mois de remboursement"
+              placeholder="Saisir le mois de remboursement de la note de frais"
+              required="Votre nom est requis"
+            />
+            <FieldTextarea
+              name="description"
+              label="Description"
+              placeholder="Saisir la description de l'objectif"
+              required="La description est requise"
+            />
+          </Stack>
+        </Formiz>
+      </PageContent>
     </Page>
   );
-};
+});
